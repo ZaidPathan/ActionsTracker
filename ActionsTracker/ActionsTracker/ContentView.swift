bd01c98787a9a9486ebf88d48db859d38ce97736
Zaid Pathan <zaidkhanintel@gmail.com>
2019-06-11 17:21:36 +0530

    HelloWorld - Initial SwiftUI Project Setup.

---
+++ ActionsTracker/ActionsTracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ActionsTracker
+//
+//  Created by Zaid Pathan on 11/06/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
